cbcda24f23b497fb2438df98b900f2b1ddec1580
Sean Hone <sean.m.hone@gmail.com>
2023-12-17 11:54:47 +1300

    Preloading font

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -7,7 +7,10 @@
 	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin="anonymous" />
 	<link
 		href="https://fonts.googleapis.com/css2?family=Rubik:wght@300;600;700;900&display=swap"
-		rel="stylesheet"
+		rel="preload"
+		as="font"
+		type="font/woff2"
+		crossorigin="anonymous"
 	/>
 </svelte:head>
 
